90563000fc5974a8b50a311dc636ab91728e3894
Miroslav Hudec <mijujda@gmail.com>
2021-10-08 11:57:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def load_requirements():
     requirements = []
     REQS_PATH = pathlib.Path(__file__).resolve().parent.joinpath('requirements.txt')
     if REQS_PATH.exists() and REQS_PATH.is_file():
-        requirements = [x for x in REQS_PATH.read_text().splitlines() if (len(x) and not r.startswith("#"))]
+        requirements = [x for x in REQS_PATH.read_text().splitlines() if (len(x) and not x.startswith("#"))]
     return requirements
 
 
